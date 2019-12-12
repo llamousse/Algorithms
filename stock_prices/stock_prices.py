bacceaf4ff9f985fa3904c7e4f1d2abdbac6f925
Vicky <vyue013@gmail.com>
2019-12-11 16:09:17 -0800

    algorithms wip

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,9 +2,49 @@
 
 import argparse
 
-def find_max_profit(prices):
-  pass
+prices = [100, 90, 80, 50, 20, 10]
 
+def find_max_profit(prices):
+  # gets all 'sold' stock prices (i.e. buy, sell, buy, sell, etc)
+  # stocks_sold_list = prices[1::2]
+  # min_value = min(stocks_sold_list)
+  # max_value = max(stocks_sold_list)
+
+  max_value = max(prices)
+  # max_profit - max_value - 0
+
+  # for x in prices:
+  if max_value == prices[0]:
+    new_stock_list = prices[1:]
+    print(new_stock_list)
+    new_max = max(new_stock_list)
+    print(new_max)
+    if new_max == prices[0]:
+      print(max_value)
+      max_profit = new_max - max_value
+      return max_profit
+  else:
+    max_value_index = prices.index(max(prices))
+
+    new_stocks_list = prices[0:max_value_index]
+    # print(new_stocks_list)
+
+    min_value = min(new_stocks_list)
+    new_max_profit = max_value - min_value
+    return new_max_profit
+
+  # max_value = max(prices)
+  # max_value_index = prices.index(max(prices))
+
+  # new_stocks_list = prices[0:max_value_index]
+  # # print(new_stocks_list)
+
+  # min_value = min(new_stocks_list)
+  # max_profit = max_value - min_value
+
+  # return max_profit
+
+print(find_max_profit(prices))
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
