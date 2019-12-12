6b8edd04e0518926ca00252a1ad5a0ea821753f1
Vicky <vyue013@gmail.com>
2019-12-12 15:55:13 -0800

    completed recipe batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,10 +1,41 @@
 #!/usr/bin/python
 
+# NOTES
+# keys - ingredient names
+# values - ingredient amounts
+# ingredients dictionary - amounts available to use
+
+# TODO
+# output max number of whole batches that can be made 
+# for supplied recipe using available ingredients
+
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
 
+  # set temp var to replace later
+  temp_batch = 500
+
+# check if length of ingredients is enough for length of recipe
+  if len(ingredients) < len(recipe):
+    return 0
+
+# loop ingredients list in recipe
+# search: iterate through dictionary (key, value)
+# key - ingredient name, value - ingredient amount
+  for key, value in recipe.items():
+    
+    # divide each ingredient amount into recipe value
+    current_batch = ingredients[key] // value
+
+    # check if current_batch is less than temp_batch
+    # finding the lowest available batch which is the maximum batch
+    if temp_batch > current_batch:
+      temp_batch = current_batch
+
+  batch = temp_batch
+
+  return batch
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
